306b33c8c18fc01dbce644a36e15daee5690d997
jacer <jacerdjo@gmail.com>
2023-04-13 08:27:52 +0100

    I made the login and signup pages and styled them with basic styling

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,5 +7,6 @@ module com.example.feedy {
     opens com.example.feedy to javafx.fxml;
     exports com.example.feedy;
     exports com.example.feedy.repositories;
+    exports com.example.feedy.controllers;
     opens com.example.feedy.repositories to javafx.fxml;
 }
